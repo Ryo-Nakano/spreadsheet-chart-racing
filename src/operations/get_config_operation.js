b361228c1e07b5c7403ea36fa19e4dd606109a4b
Ryo-Nakano <matsuhara.9999ryo@gmail.com>
2025-11-23 11:35:25 +0900

    refactor: シートデータクラスの配置を bound_sheet 配下に変更 #15

--- src/operations/get_config_operation.js
+++ src/operations/get_config_operation.js
@@ -1,5 +1,5 @@
 import { BaseOperation } from "base_classes/base_operation";
-import { Config } from "sheet_data/config";
+import { Config } from "sheet_data/bound_sheet/config";
 
 export class GetConfigOperation extends BaseOperation {
   _operation() {
